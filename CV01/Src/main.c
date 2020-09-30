2aeb6dfd4bd98b1cefc1cda4719e634b9189558d
xstola02 <xstola02@vutbr.cz>
2020-09-30 11:20:55 +0200

    CV01 before comments

--- CV01/Src/main.c
+++ CV01/Src/main.c
@@ -17,12 +17,31 @@
  ******************************************************************************
  */
 
+#include "stm32f0xx.h"
+
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
 int main(void)
 {
+
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;				//
+	GPIOA->MODER |= GPIO_MODER_MODER5_0;			//
+
     /* Loop forever */
-	for(;;);
+	for(;;)
+	{
+		uint32_t pole = 0b10101001110111011100101010000000 ;  // field filled with binary number we want
+
+		for (uint8_t i = 0; i < sizeof(pole); i++)
+		{
+			if(pole & 0x80) GPIOA->BSRR = (1<<5); 			// set
+			else GPIOA->BRR = (1<<5); 						//reset
+
+			pole <<=1;
+
+			for (volatile uint32_t i = 0; i < 100000; i++) {}
+		}
+	}
 }
