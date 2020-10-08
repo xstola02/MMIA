4ae652fb5db54aa5ff162036266d647f32a8a40a
xstola02 <xstola02@vutbr.cz>
2020-10-08 19:12:57 +0200

    CV02 obsluha tlacitek

--- CV02/Src/main.c
+++ CV02/Src/main.c
@@ -6,6 +6,12 @@
  ******************************************************************************
 **/
 #include "stm32f0xx.h"
+#define LED_TIME_BLINK 300
+#define BUTTON_DEBOUNCE 40
+#define LED_TIME_SHORT 100
+#define LED_TIME_LONG 1000
+
+static volatile uint32_t Tick;
 
 
 
@@ -19,9 +25,76 @@ void EXTI0_1_IRQHandler(void)
 	if (EXTI->PR & EXTI_PR_PR0)  // check line 0 has triggered the IT
 	{
 		EXTI->PR |= EXTI_PR_PR0; // clear the pending bit
-		GPIOB->ODR ^=  (1<<4) ;
+		GPIOB->ODR ^= (1<<4);
     }
  }
+
+
+void SysTick_Handler(void)
+ {
+	 Tick++;
+ }
+
+
+void blikac(void)
+ {
+ 	static uint32_t delay;
+
+ 	if (Tick > delay + LED_TIME_BLINK)
+	{
+
+	 GPIOA->ODR ^= (1<<4);
+	 delay = Tick;
+
+	}
+ }
+
+
+void tlacitka(void)
+{
+	static uint32_t debounce1;
+	static uint32_t off_time;
+	static uint32_t on_time;
+
+	if (Tick > debounce1 + BUTTON_DEBOUNCE)
+	{
+		static uint32_t old_s2;						// stary stav tlacitka
+		static uint32_t old_s1;
+		uint32_t new_s1 = GPIOC->IDR & (1<<0);
+		uint32_t new_s2 = GPIOC->IDR & (1<<0);		// aktualni stav tlacitka
+
+
+		if (old_s2 && !new_s2) 						// falling edge (in stary stav je true a novy stav je false -> detekce sestupne hrany
+			{
+				off_time = Tick + LED_TIME_SHORT;
+				GPIOB->BSRR = (1<<0);
+
+			}
+		old_s2 = new_s2; //ukladani noveho stavu do toho stareho
+
+		if (old_s1 && !new_s1)
+		{
+			on_time= Tick + LED_TIME_LONG;
+			GPIOB -> BSRR = (1<<0);
+		}
+		old_s1 = new_s1;
+
+	}
+
+	if (Tick > off_time)
+	{
+	 GPIOB->BRR = (1<<0);
+	}
+
+	if(Tick >on_time)
+	{
+		GPIOB->BRR = (1<<0);
+	}
+	debounce1 = Tick;
+}
+
+
+
 
 int main(void)
 {
@@ -32,17 +105,28 @@ int main(void)
 	 GPIOC->PUPDR |= GPIO_PUPDR_PUPDR1_0; // S1 = PC1, pullup
 
 	 RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
+	 SysTick_Config(8000); // 1ms -> hodiny 8MHz -> 8 000 000 Hz -> 8 000 = 1ms
 
-
+ /*
 	 SYSCFG->EXTICR[0] |= SYSCFG_EXTICR1_EXTI0_PC; // select PC0 for EXTI0
 	 EXTI->IMR |= EXTI_IMR_MR0; // mask
 	 EXTI->FTSR |= EXTI_FTSR_TR0; // trigger on falling edge
 	 NVIC_EnableIRQ(EXTI0_1_IRQn); // enable EXTI0_1
+*/
 
-    /* Loop forever */
+	 /* Loop forever */
 	for(;;)
 	{
-
+		blikac();
+		tlacitka();
 	}
 
 }
+
+
+
+
+
+
+
+
