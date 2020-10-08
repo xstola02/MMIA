090d18d1eaaa69057cf7bf5eb567a310a24a148f
xstola02 <xstola02@vutbr.cz>
2020-10-08 16:39:47 +0200

    CV02 bod 2.2

--- CV02/Src/main.c
+++ CV02/Src/main.c
@@ -14,6 +14,15 @@
 #endif
 
 
+void EXTI0_1_IRQHandler(void)
+ {
+	if (EXTI->PR & EXTI_PR_PR0)  // check line 0 has triggered the IT
+	{
+		EXTI->PR |= EXTI_PR_PR0; // clear the pending bit
+		GPIOB->ODR ^=  (1<<4) ;
+    }
+ }
+
 int main(void)
 {
 	 RCC->AHBENR |= RCC_AHBENR_GPIOAEN | RCC_AHBENR_GPIOBEN | RCC_AHBENR_GPIOCEN; // enable
@@ -23,6 +32,12 @@ int main(void)
 	 GPIOC->PUPDR |= GPIO_PUPDR_PUPDR1_0; // S1 = PC1, pullup
 
 	 RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
+
+
+	 SYSCFG->EXTICR[0] |= SYSCFG_EXTICR1_EXTI0_PC; // select PC0 for EXTI0
+	 EXTI->IMR |= EXTI_IMR_MR0; // mask
+	 EXTI->FTSR |= EXTI_FTSR_TR0; // trigger on falling edge
+	 NVIC_EnableIRQ(EXTI0_1_IRQn); // enable EXTI0_1
 
     /* Loop forever */
 	for(;;)
