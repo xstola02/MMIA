06c15b47e9feb34b9d52c193e481b1a6ffa7d111
xstola02 <xstola02@vutbr.cz>
2020-10-25 18:49:02 +0100

    CV04 ukol 2.2

---
+++ CV04/Core/Inc/sct.h
@@ -0,0 +1,15 @@
+/*
+ * sct.h
+ *
+ *  Created on: 15. 10. 2020
+ *      Author: Admin
+ */
+
+#ifndef SCT_H_
+#define SCT_H_
+
+void sct_led(uint32_t value);
+void sct_init(void);
+void sct_value (uint16_t value,uint8_t led);
+
+#endif /* SCT_H_ */
---
+++ CV04/Core/Src/sct.c
@@ -0,0 +1,123 @@
+/*
+ * sct.c
+ *
+ *  Created on: 15. 10. 2020
+ *      Author: Hana Stolarova
+ */
+
+//including all .h files
+#include "stm32f0xx.h"
+#include  "sct.h"
+#include "main.h"
+
+//defining functions/macros so we can set register signals
+
+void sct_led(uint32_t value)
+{
+	for (uint8_t j = 0; j<32; j++)
+	{
+		HAL_GPIO_WritePin(SCT_SDI_GPIO_Port, SCT_SDI_Pin, (value & 1));  //assigning value 1 to sct_sdi using HAL command
+
+		// sct_sdi(value & 1);
+		value >>=1;
+		HAL_GPIO_WritePin(SCT_CLK_GPIO_Port, SCT_CLK_Pin, 1); //assigning value 1 to sct_clk using HAL command
+		HAL_GPIO_WritePin(SCT_CLK_GPIO_Port, SCT_CLK_Pin, 0);
+	}
+
+	HAL_GPIO_WritePin(SCT_NLA_GPIO_Port, SCT_NLA_Pin, 1); //assigning value 1 to sct_nla using HAL command
+	HAL_GPIO_WritePin(SCT_NLA_GPIO_Port, SCT_NLA_Pin, 0);
+
+	//sct_nla(1);
+	//sct_nla(0);
+
+}
+
+
+void sct_init(void)
+{
+	HAL_GPIO_WritePin(SCT_NOE_GPIO_Port, SCT_NOE_Pin, 0); //zeros into registers
+	//sct_noe(0);
+	sct_led(0);			//output activation using neg.OE
+
+}
+
+void sct_value (uint16_t value,uint8_t led) //function uses value, renders its value into segments which then displayson the board
+{
+	static const uint32_t reg_values[4][10] =				//translation of numbers into segments
+	{
+	 {
+	     //PCDE--------GFAB @ DIS1 							//this part tells which segments are ment to light up for specific numbers
+		 0b0111000000000111 << 16,
+		 0b0100000000000001 << 16,
+		 0b0011000000001011 << 16,
+		 0b0110000000001011 << 16,
+		 0b0100000000001101 << 16,
+		 0b0110000000001110 << 16,
+		 0b0111000000001110 << 16,
+		 0b0100000000000011 << 16,
+		 0b0111000000001111 << 16,
+		 0b0110000000001111 << 16,
+	 },
+	 {
+		 //----PCDEGFAB---- @ DIS2
+		 0b0000011101110000 << 0,
+		 0b0000010000010000 << 0,
+		 0b0000001110110000 << 0,
+		 0b0000011010110000 << 0,
+		 0b0000010011010000 << 0,
+		 0b0000011011100000 << 0,
+		 0b0000011111100000 << 0,
+		 0b0000010000110000 << 0,
+		 0b0000011111110000 << 0,
+		 0b0000011011110000 << 0,
+	 },
+	 {
+		 //PCDE--------GFAB @ DIS3
+		 0b0111000000000111 << 0,
+		 0b0100000000000001 << 0,
+		 0b0011000000001011 << 0,
+		 0b0110000000001011 << 0,
+		 0b0100000000001101 << 0,
+		 0b0110000000001110 << 0,
+		 0b0111000000001110 << 0,
+		 0b0100000000000011 << 0,
+		 0b0111000000001111 << 0,
+		 0b0110000000001111 << 0,
+	 },
+	 {
+		 //----43215678---- @ LED
+		 0b0000000000000000 << 16,
+		 0b0000000100000000 << 16,
+		 0b0000001100000000 << 16,
+		 0b0000011100000000 << 16,
+		 0b0000111100000000 << 16,
+		 0b0000111110000000 << 16,
+		 0b0000111111000000 << 16,
+		 0b0000111111100000 << 16,
+		 0b0000111111110000 << 16,
+
+	 },
+	 };
+
+	uint32_t reg = 0;
+
+	reg |= reg_values[0][value/100 % 10];		//value of the position of hundreds
+	reg |= reg_values[1][value/10 % 10];		//value of the position of tens
+	reg |= reg_values[2][value/1 % 10];			//value of the position of units
+	reg |= reg_values[3][led];					//bargraf
+
+	sct_led(reg); 			//enlisting final value into register
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
