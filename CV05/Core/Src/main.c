92e7b226691d894881f71332fad11e0971430644
xstola02 <xstola02@vutbr.cz>
2020-11-05 18:57:16 +0100

    CV05 posledni bod a komentare

--- CV05/Core/Src/main.c
+++ CV05/Core/Src/main.c
@@ -44,12 +44,15 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c1;
+
 UART_HandleTypeDef huart2;
 DMA_HandleTypeDef hdma_usart2_rx;
 
 /* USER CODE BEGIN PV */
 #define CMD_BUFFER_LEN 64
 #define RX_BUFFER_LEN 64
+#define EEPROM_ADDR 0xA0
 
 static uint8_t uart_rx_buf[RX_BUFFER_LEN];
 static volatile uint16_t uart_rx_read_ptr = 0;
@@ -63,6 +66,7 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_DMA_Init(void);
 static void MX_USART2_UART_Init(void);
+static void MX_I2C1_Init(void);
 /* USER CODE BEGIN PFP */
 
 static void uart_process_command(char *cmd)
@@ -120,6 +124,49 @@ static void uart_process_command(char *cmd)
 
 	}
 	//_____________________________________________
+
+	else if (strcasecmp(token, "READ") == 0) 			//pokud napiseme "READ" precte z adresy ulozenou hodnotu
+		{
+		token = strtok(NULL, " ");
+		uint16_t adr = atoi(token);
+		uint8_t value ;
+
+		HAL_I2C_Mem_Read(&hi2c1, EEPROM_ADDR, adr, I2C_MEMADD_SIZE_16BIT, &value, 1, 1000);
+		printf("Adresa 0x%04X = 0x%02X\n ", adr, value);
+
+		}
+
+	else if (strcasecmp(token, "WRITE") == 0) 			//pokud napiseme "WRITE" zapise do pameti na urcitou adresu urcitou hodnotu
+		{
+		token = strtok(NULL, " ");
+		uint16_t adr = atoi(token);
+		token = strtok(NULL, " ");
+		uint8_t value = atoi(token);
+
+
+
+		HAL_I2C_Mem_Write(&hi2c1, EEPROM_ADDR, adr, I2C_MEMADD_SIZE_16BIT, &value, 1, 1000);
+		/* Check if the EEPROM is ready for a new operation */
+			while (HAL_I2C_IsDeviceReady(&hi2c1, EEPROM_ADDR, 300, 1000) == HAL_TIMEOUT)
+			{}
+
+
+
+		}
+
+	else if (strcasecmp(token, "DUMP") == 0) 			//pokud napiseme "DUMP"
+			{
+			uint16_t memory[16];
+			uint8_t i;
+
+			for (i = 0; i < 16; i++) 	//naplni pole memory postupne hodnotami a zaroven kazdou adresu vypise
+				{
+				HAL_I2C_Mem_Read(&hi2c1, EEPROM_ADDR, i, I2C_MEMADD_SIZE_16BIT, &memory[i], 16, 1000);
+				printf( "0x%02X\n", memory[i]);
+				}
+			}
+
+
 
 	else
 	{
@@ -192,6 +239,7 @@ int main(void)
   MX_GPIO_Init();
   MX_DMA_Init();
   MX_USART2_UART_Init();
+  MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
 
   HAL_UART_Receive_DMA(&huart2, uart_rx_buf, RX_BUFFER_LEN);
@@ -234,6 +282,7 @@ void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
@@ -261,6 +310,58 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_I2C1;
+  PeriphClkInit.I2c1ClockSelection = RCC_I2C1CLKSOURCE_HSI;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.Timing = 0x2000090E;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure Analogue filter
+  */
+  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c1, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure Digital filter
+  */
+  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c1, 0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
+
 }
 
 /**
